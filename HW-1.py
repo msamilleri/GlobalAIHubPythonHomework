27da5fd3b2d3465712d33cea213d1e6008fe52b4
Mustafa Şamil İleri <44174659+ileri95@users.noreply.github.com>
2020-12-27 03:34:30 +0300

    Add files via upload

---
+++ HW-1.py
@@ -0,0 +1,13 @@
+data1=int(input("Data-1 :"))
+data2=input("Data-2 :")
+data3=input("Data-3 :")
+data4=input("Data-4 :")
+data5=float(input("Data-5 :"))
+
+print("data1 =",data1,"data1 type ",type(data1))
+print("data2 ={} data typr {}".format(data2,type(data2)))
+print(f'data3 = {data3} data type {type(data3)}')
+print("data4 ={} data type {}".format(data4,type(data4)))
+print("data 5= " + str(data5),"data type",str(type(data5)) )
+
+
